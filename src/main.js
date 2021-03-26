3361de041eada86bdde50bc6f414f4c74d6f1be2
João Marcos <joaomarcos.src@gmail.com>
2021-03-26 19:15:43 -0300

    fix(src) remove items

--- src/main.js
+++ src/main.js
@@ -4,18 +4,13 @@ import {
 import App from "./App"
 import router from "./router"
 import store from "./store"
-// import './assets/element-variables.scss'
 import installElementPlus from './plugins/element'
 import './css/main.css'
-import LoadingComponent from "./components/util/LoadingComponent.vue"
-
 
 const app = createApp(App);
 
 app.use(store)
 app.use(router)
 app.use(installElementPlus)
-
-app.component('loading-component', LoadingComponent)
 
 app.mount("#app");
